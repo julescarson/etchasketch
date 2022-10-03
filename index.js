767949b8aeef449c1cc9a8ff3c66d5d6a1b17e81
MetaMoniker <metamoniker@gmail.com>
2022-10-02 22:59:04 -0400

    divs added

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const content = document.querySelector('.content');
+const x = 16;
+const y = 16;
 
 
+
+const makedivs = (parent, num, cn) => {
+    return Array.from(Array(num)).map((e, i) => {
+        const nd = document.createElement('div');
+        nd.className = i;
+        nd.classList.add(cn);
+        parent.append(nd);
+        return nd;
+    });
+}
+
+const r = makedivs(content, x, 'x');
+
+for (const topLevelDiv of r) {
+    makedivs(topLevelDiv, y, 'y');
+}
+
+console.log(r[0][0]);
