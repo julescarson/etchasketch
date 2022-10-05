c73869be07b33399463ff4bfe5a85b5d05572538
MetaMoniker <metamoniker@gmail.com>
2022-10-04 23:02:42 -0400

    erase button added

--- index.js
+++ index.js
@@ -1,42 +1,67 @@
+
+
 const content = document.querySelector('.content');
-const gridContainer = document.getElementsByClassName('content');
-const x = 256;
-const userColor = `black`;
 
+//grid
+const grid = document.createElement('div');
+grid.classList.add('grid16');
+content.appendChild(grid);
 
+// UI
+const uiElements = document.createElement('div');
+uiElements.classList.add('ui');
+content.appendChild(uiElements);
 
+//total grid squares (256 inital)
+let x = 256;
+
+//create divs for grid
 for (i = 0; i <= x; i++) {
     const newDiv = document.createElement(`div`)
-    newDiv.setAttribute("id", i)
+    newDiv.setAttribute("id", "sqr")
     newDiv.classList.add("x")
-    content.appendChild(newDiv);
+    grid.appendChild(newDiv);
 }
 
+//access grid squares
 const squares = document.getElementsByClassName(`x`);
 
+//mouse clicked or unclicked
 let mouseClicked = false;
 document.body.onmousedown = () => mouseClicked = true;
 document.body.onmouseup = () => mouseClicked = false;
 
-const makeActive = (e) => {
+//draw when clicked
+const firstClick = (e) => {
+    e.target.setAttribute("class", "x xactive");
+}
+const drawOn = (e) => {
     if (mouseClicked == true) {
-        e.target.setAttribute("class", "xactive");
+        e.target.setAttribute("class", "x xactive");
     }
 }
 
+//listeners for when to draw - array of grid squares
 Array.from(squares).forEach(function (squares) {
-    squares.addEventListener('mouseenter', makeActive);
+    squares.addEventListener('mousedown', firstClick);
+    squares.addEventListener('mouseenter', drawOn);
 });
 
 
-
-
-
 // color picker
-
+const userColor = `black`;
 // canvas size changer (presets)
 
 // erase (clear) drawing
+const eraseButton = document.createElement('button');
+eraseButton.classList.add('erase');
+eraseButton.textContent = `Erase All`;
+eraseButton.onclick = () => {
+    document.querySelectorAll(`.x`).forEach(e => { e.classList.remove('xactive') });
+}
+
+uiElements.appendChild(eraseButton);
+
 
 // page style
 
